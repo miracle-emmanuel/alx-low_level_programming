36c86ba478590300914fea228c949f050d64fb42
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-02-21 14:49:02 -0800

    ./4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *isalpha- Checks for alphabetic character
+ *@c: The character to be checked
+ *Return: 1 for alphabetic character or for anything else
+*/
+int isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
+
