286e95c7726e218df162b2491f823a4f32950968
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-02-22 01:50:56 -0800

    ./11-98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print all number from n to 98
+ * @n: starting point
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 90)
+	{
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+}
