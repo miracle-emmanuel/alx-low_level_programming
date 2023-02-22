2495a2083fe5f887b78825a2abac043ef2f1ec45
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-02-22 14:27:31 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests functions that prints if integer is positive or negative
+ * Return: 0
+ */
+
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	positive_or_nagative(i);
+
+	return (0);
+}
