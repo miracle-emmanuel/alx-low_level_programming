fb758491570e19f511ec46cedff7b0dae6b313d9
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-02-22 05:19:22 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long halfi, half2;
+	unsigned long half1, half2;
 
 	for (count = 0; count < 92; count++)
 	{
@@ -27,11 +27,11 @@ int main(void)
 	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
-	half2 fib1_half2 + fib2 half2;
+	half2 = fib1_half2 + fib2 half2;
 	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
 	half1 += 1;
-	half2 % 10000000000;
+	half2 %= 10000000000;
 	}
 	printf("%lu%lu", half1, half2);
 	if (count != 98)
