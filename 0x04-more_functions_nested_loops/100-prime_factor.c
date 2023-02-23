ed0380b632df28ab21c9018a4bee39fa951f469e
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-02-23 14:46:57 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * prime_factor - print the prime factor
+ * return 0(success)
+ */
+
+int main(void)
+{
+
+	long x, maxf;
+	lomg numbr = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+	if (number % x == 0)
+	{
+	if maxf = number / x;
+	}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
