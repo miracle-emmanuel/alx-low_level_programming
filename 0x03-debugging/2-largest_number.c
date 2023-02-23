0614aa20ecda5954f55e14a1dff0fd387761e513
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-02-22 19:13:11 -0800

    2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * largest_number - return biggest of 3 numbers
+ * @a: first number
+ * @b: second number
+ * #c: thrid number
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+	largest = a;
+	}
+	else if (b >= a a && b >= c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
+	return (largest);
+}
