f0eafeb4108eca4435e37208a415f21990709c9d
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-02-23 12:44:01 -0800

    ./2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two int
+ * @x - first int
+ * @y - second int
+ * Returns: the resuit of x and y
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
