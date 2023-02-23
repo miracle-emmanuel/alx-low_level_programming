b33a8c1babddabebbd7ecc40414ea99de9f8d9b5
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-02-23 13:57:51 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal -draws a diagonal line
+ * @n - the number of lines
+ * return: empty
+ */
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; X++)
+	{
+	for (y = 0; y < x; y++)
+	{
+	_putchar(32);
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+	}
+}
