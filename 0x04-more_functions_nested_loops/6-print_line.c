28c843e3ea0c7526bca2c31c1cdbc2b395b77f17
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-02-23 13:38:27 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: the number of lines
+ * return :empty
+ */
+
+void print_line(int n)
+
+{
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; X++)
+	{
+	_putchat(95);
+	}
+	_putchar('\n');
+	}
+}
