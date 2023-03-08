458d658d021513bb0caf9217f57ee8a0ebf6e6bb
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-03-08 13:37:17 -0800

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - fact of any number
+ * @n: number
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	int f;
+
+	if (n < 0)
+		return (-1);
+	else if (n <= 1)
+		return (1);
+	f = n * factorial(n - 1);
+	return (f);
+}
