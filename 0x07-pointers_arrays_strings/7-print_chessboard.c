eca6c9b2c2d2cf120cde05584bb7f744e10fbd2a
Miracle Emmanuel <miracleemmanuel9952gmail.com>
2023-03-08 12:54:48 -0800

    0-main.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - entry point
+ * @a: array
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int c, b;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (b = 0; b < 8; b++)
+			_putchar(a[c][b]);
+		_putchar('\n');
+	}
+}
